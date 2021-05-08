8e5348561c3af47f9195dab9a044dbaac3ef80bb
Maxim <moorcore@users.noreply.github.com>
2021-05-08 14:12:11 +0200

    init commit

---
+++ src/main/java/org/moorcore/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -0,0 +1,42 @@
+package org.moorcore.spring.springboot.springboot_rest.dao;
+
+import org.moorcore.spring.springboot.springboot_rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Query query = entityManager.createQuery("from Employee");
+
+        return (List<Employee>) query.getResultList();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Query query = entityManager.createQuery("delete from Employee " +
+                "where id=:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
+}
---
+++ src/test/java/org/moorcore/spring/springboot/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moorcore.spring.springboot.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
